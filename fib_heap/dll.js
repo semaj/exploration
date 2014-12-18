91c1c7e4750e33c6dba878fb900954e88b4ce8b3
James Larisch <root@jameslarisch.com>
2014-12-18 16:31:30 -0500

    complete DoubleLinkedList#new and #add

--- fib_heap/dll.js
+++ fib_heap/dll.js
@@ -1,8 +1,26 @@
 function DoubleLL(root) {
-  this.list = new Node(true, root);
+  this.root = root;
+  this.min = null;
+  this.size = 0;
 }
 
-function Node(is_first, root, previous, next) {
+DoubleLL.prototype.add = function(key, value) {
+  if (this.size === 0) {
+    this.size = 1;
+    this.min = new Node(true, key, value);
+  } else {
+    this.size++;
+    var newNode = new Node(false, key, value, this.min, this.min.next);
+    this.min.next.previous = newNode;
+    this.min.next = newNode;
+    if (key < this.min.key) {
+      this.min = newNode;
+    }
+  }
+  return this.min;
+};
+
+function Node(is_first, key, value, previous, next) {
   if (is_first) {
     this.previous = this;
     this.next = this;
@@ -10,11 +28,10 @@ function Node(is_first, root, previous, next) {
     this.previous = previous;
     this.next = next;
   }
-  this.root = root;
+  this.key = key;
+  this.value = value;
   this.num_children = 0;
-  this.children = new DoubleLL();
   this.marked = false;
 }
-
 
 
--- fib_heap/test.js
+++ fib_heap/test.js
@@ -1,8 +1,44 @@
 
 QUnit.test( "Node#new", function( assert ) {
-  var n = Node(true, 15);
+  var n = new Node(true, 15);
   assert.deepEqual(n.previous, n, "First node's next is itself");
   assert.deepEqual(n.next, n, "First node's previous is itself");
+});
+
+QUnit.test( "DoubleLL#new", function( assert ) {
+  var d = new DoubleLL(0);
+  assert.deepEqual(d.size, 0, "New DLL should be empty");
+});
+
+QUnit.test( "DoubleLL#add", function( assert ) {
+  var d = new DoubleLL(0);
+  d.add(15, "blah");
+  assert.deepEqual(d.size, 1, "Should not be empty after first addition");
+  assert.deepEqual(d.min.key, 15, "Should assign min (key test) to new node after first addition");
+  assert.deepEqual(d.min.value, "blah", "Should assign min (value test) to new node after first addition");
+  d.add(14, "else");
+  assert.deepEqual(d.size, 2, "Should not be empty after second addition");
+  assert.deepEqual(d.min.key, 14, "Should reassign min after lower, second addition");
+  assert.deepEqual(d.min.value, "else", "Should reassign min value after lower, second addition");
+});
+
+QUnit.test( "DoubleLL#add correctly swaps", function( assert ) {
+  var d = new DoubleLL(0);
+  var a = d.add(3, "A");
+  var b = d.add(2, "B");
+  assert.deepEqual(b.previous, a, "Second's previous should be first");
+  assert.deepEqual(b.next, a, "Second's previous should be first");
+  assert.deepEqual(a.next, b, "First's next should be second");
+  assert.deepEqual(a.previous, b, "First's previous should be second");
+  assert.deepEqual(d.min, b, "D's min is B");
+  var c = d.add(1, "C");
+  assert.deepEqual(b.previous, a, "Second's previous still first");
+  assert.deepEqual(b.next, c, "Second's next should be third");
+  assert.deepEqual(c.previous, b, "Third's previous should be second");
+  assert.deepEqual(c.next, a, "Third's next should be first");
+  assert.deepEqual(a.next, b, "First's next should be second");
+  assert.deepEqual(a.previous, c, "First's previous should be third");
+  assert.deepEqual(d.min, c, "D's min is C");
 });
 
 // FibHeap tests
